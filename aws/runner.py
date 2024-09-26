1ea356ac5350b5635b79303d94d7ded35ec8204a
Krzysztof Romanowski <castus.pl@gmail.com>
2024-09-26 19:00:39 +0200

    Changes

--- aws/runner.py
+++ aws/runner.py
@@ -1,7 +1,8 @@
 from qiskit_braket_provider import BraketLocalBackend
 qpu_backend = BraketLocalBackend()
 
-def run(qc):
-    qpu_task = qpu_backend.run(qc, shots=100)
+def run(qc, shots=100):
+    qpu_task = qpu_backend.run(qc, shots=shots)
     data = qpu_task.result()
     print(data.get_counts())
+    return qpu_task
